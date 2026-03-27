ec6f4f7f87d2903a14963296bcfe451f7b7fdd26
Okechukwu Obiajunwa <okey.obia@gmail.com>
2026-03-27 12:21:38 -0500

    fix: update references to aws_secretsmanager_secret in database module

--- terraform/modules/database/main.tf
+++ terraform/modules/database/main.tf
@@ -1,9 +1,10 @@
-resource "aws_secretsmanager_secret" "db" {
-  name = "${var.app_name}-db"
+
+data "aws_secretsmanager_secret" "db" {
+  name = "${var.app_name}/db"
 }
 
-resource "aws_secretsmanager_secret_version" "db" {
-  secret_id = aws_secretsmanager_secret.db.id
+data "aws_secretsmanager_secret_version" "db" {
+  secret_id = data.aws_secretsmanager_secret.db.id
 }
 
 locals {
--- terraform/modules/database/outputs.tf
+++ terraform/modules/database/outputs.tf
@@ -1,4 +1,4 @@
 output "db_endpoint" { value = aws_db_instance.db.endpoint }
 output "secret_arn" {
-     value = aws_secretsmanager_secret.db.arn 
+     value = data.aws_secretsmanager_secret.db.arn
 }
