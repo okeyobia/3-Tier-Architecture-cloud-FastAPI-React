c1bf735fd0dd0b3c364a562aaf91afbcd3befb1d
Okechukwu Obiajunwa <okey.obia@gmail.com>
2026-03-27 19:12:10 -0500

    fix: comment out CloudFront URL output in outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,6 +2,6 @@ output "alb_dns" {
   value = module.backend.alb_dns
 }
 
-output "cloudfront_url" {
-  value = module.frontend.cloudfront_url
-}
+# output "cloudfront_url" {
+#   value = module.frontend.cloudfront_url
+# }
